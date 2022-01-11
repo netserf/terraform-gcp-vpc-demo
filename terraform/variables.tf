d2d6035ce18279f31e5a06a716e398a3b2454b29
Greg Horie <networkserf@gmail.com>
2022-01-11 08:56:22 -0800

    Initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "The name of the region where network is being created"
+  type        = string
+}
+
+variable "project_id" {
+  description = "The ID of the project"
+  type        = string
+}
+
