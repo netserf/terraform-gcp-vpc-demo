45e9b8ea50b102702e6bab6149f449f2e2e6171a
Greg Horie <networkserf@gmail.com>
2022-01-14 09:01:31 -0800

    fix: variable naming and comments (#1)
    * fix variable naming mistake to avoid confusion
    
    * remove unneeded comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-resource "google_compute_subnetwork" "tf-custom-net" {
+resource "google_compute_subnetwork" "my-subnet" {
   name          = "${local.name_suffix}-subnet"
-  ip_cidr_range = "10.0.0.0/24
+  ip_cidr_range = "10.0.0.0/24"
   region        = "us-west1"
-  network       = google_compute_network.tf-subnet.id
+  network       = google_compute_network.my-net.id
 }
 
-resource "google_compute_network" "tf-subnet" {
+resource "google_compute_network" "my-net" {
   name                    = "${local.name_suffix}-net"
   auto_create_subnetworks = false
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,3 @@
-# This file has some scaffolding to make sure that names are unique and that
-# a region and zone are selected when you try to create your Terraform resources.
-
 locals {
   name_suffix = "tf-trial"
 }
